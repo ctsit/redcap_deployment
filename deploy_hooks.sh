03166018ad373d0fd0d783857141066baffdb4ae
Erik C. Schmidt <cavedivr@ufl.edu>
2015-07-02 13:37:08 -0400

    Add error checking for presence and creation of hooks directory

--- deploy_hooks.sh
+++ deploy_hooks.sh
@@ -8,6 +8,11 @@ if [ ! -e $REDCAP_ROOT ]; then
     echo "Error: REDCAP_ROOT, $REDCAP_ROOT, does not exist.  Exiting."
     exit
 fi
+if [ ! -e $REDCAP_HOOKS ]; then
+    mkdir $REDCAP_ROOT/hooks
+    echo "REDCap Hooks Directory Created."
+    exit
+fi
 
 # Pull repo and copy scripts into library folder
 MYTEMP=`mktemp -d`
@@ -17,8 +22,8 @@ if [ ! -e redcap-extras ]; then
     echo "Error: redcap-extras repo could not be cloned. Exiting."
     exit
 fi
-
 cd redcap-extras/hooks
+
 # checkout develop because we have not yet released the code we need
 git checkout develop
 cp redcap_hooks.php $REDCAP_HOOKS/
@@ -31,5 +36,3 @@ awk -F"," 'NR!=1{printf "mkdir -p %s/%s/%s/\n",ENVIRON["REDCAP_HOOKS"],$1,$2}' $
 
 # Create sym links for hooks to be executed
 awk -F"," 'NR!=1{printf "ln -s %s/%s %s/%s/%s/\n",ENVIRON["REDCAP_HOOKS"],$3,ENVIRON["REDCAP_HOOKS"],$1,$2}' $INPUT | sh
-
-#
