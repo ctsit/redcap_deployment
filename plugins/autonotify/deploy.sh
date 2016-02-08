3757d24cf0579da48858db25e323847ffe2d913c
Philip Chase <pbc@ufl.edu>
2016-02-08 16:33:07 -0500

    Adjust log directory ownership and add logrotation for auto-notify plugin

--- plugins/autonotify/deploy.sh
+++ plugins/autonotify/deploy.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 set -e
 
-# Make the required log directory
+# Make the required log directory and give the web server write access
 mkdir -p /var/log/redcap/autonotify
+chown -R www-data.www-data /var/log/redcap
+
+# Activate log rotation to assure we never have to worry about these logs
+cat << EOF > /etc/logrotate.d/redcap-autonotify
+/var/log/redcap/autonotify/*.log {
+    weekly
+    missingok
+    rotate 52
+    compress
+    delaycompress
+    notifempty
+    create 640 www-data www-data
+    sharedscripts
+}
+EOF
