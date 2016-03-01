f3f77999599888bb6a5b72c30183161ddf4fd4b0
Philip Chase <pbc@ufl.edu>
2016-03-01 09:07:14 -0500

    For autonotify plugin, add file copy tasks to deploy.sh and fix path to log file

--- plugins/autonotify/deploy.sh
+++ plugins/autonotify/deploy.sh
@@ -1,13 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
+export MYTARGETDIR=$1
+
+# determine the directory where this script resides
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# copy files to the correct target locations
+mkdir -p $MYTARGETDIR
+cp $DIR/index.php $MYTARGETDIR
+cp $DIR/common.php $MYTARGETDIR
+
 # Make the required log directory and give the web server write access
-mkdir -p /var/log/redcap/autonotify
+mkdir -p /var/log/redcap
 chown -R www-data.www-data /var/log/redcap
 
 # Activate log rotation to assure we never have to worry about these logs
 cat << EOF > /etc/logrotate.d/redcap-autonotify
-/var/log/redcap/autonotify/*.log {
+/var/log/redcap/autonotify*.log {
     weekly
     missingok
     rotate 52
