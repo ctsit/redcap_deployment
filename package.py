5556c58090178f921d672444f655ed3fde202a22
marlycormar <marlycormar@gmail.com>
2017-05-26 10:49:09 -0400

    Change language deployment configuration

--- package.py
+++ package.py
@@ -128,14 +128,18 @@ def deploy_extension_to_build_space(source_dir="", build_target=""):
 
 
 def deploy_language_to_build_space():
-    target_dir = env.builddir + "/redcap/languages/"
-    for language in json.loads(env.languages):
-        if os.path.exists(language):
-            local("mkdir -p %s" % target_dir)
-            local('cp %s %s' % (language, target_dir))
+    source_dir = env.languages
+    target_dir = env.builddir + "/redcap/languages"
+    if  local('find %s/*.ini -maxdepth 1 -type f | wc -l' % source_dir, capture = True) != '0':
+        if os.path.exists(source_dir) and os.path.exists(target_dir):
+            local('find %s/*.ini -maxdepth 1 -type f -exec rsync {} %s \;' %(source_dir, target_dir))
+            #local('find %s/*.ini -maxdepth 1 -type f -exec cp {} %s \\' % source_dir)
+            #find myDir -name "*.html" -exec cp {} ~/otherDir \;
+            #local('cp %s %s' % (source_dir + "/*.ini", target_dir))
+        elif os.path.exists(source_dir):
+            abort("the languages folder %s does not exist" % target_dir)
         else:
-            abort("the language file %s does not exist" % language)
-
+            abort("the languages folder %s does not exist" % source_dir)
 
 def apply_patches():
     for repo in json.loads(env.patch_repos):
